831d0d027dfd88b2f0ffa4fb542270c44fd10c78
Glitch (ecr-url-shortener-microservice) <none>
2019-12-14 13:44:30 +0000

    📒🏎 Checkpoint ./server.js:9298160/9448

--- server.js
+++ server.js
@@ -1,58 +1,34 @@
 "use strict";
 
-var express = require("express");
-var mongo = require("mongodb");
-var mongoose = require("mongoose");
-var bodyParser = require("body-parser");
-var dns = require("dns");
-
-var cors = require("cors");
-
-var app = express();
-
-// Basic Configuration
-var port = process.env.PORT || 3000;
-
-/** this project needs a db !! **/
+const express = require("express");
+const mongo = require("mongodb");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const dns = require("dns");
+const cors = require("cors");
+const app = express();
+const port = process.env.PORT || 3000;
 
 mongoose
   .connect(process.env.MONGOLAB_URI, { useNewUrlParser: true })
   .catch(error => console.log(error));
 console.log("mongoose connection readyState", mongoose.connection.readyState);
-app.use(cors()); //
-
-/** this project needs to parse POST bodies **/
-// you should mount the body-parser here
+app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
-
 app.use("/public", express.static(process.cwd() + "/public"));
-
 app.get("/", function(req, res) {
   res.sendFile(process.cwd() + "/views/index.html");
 });
 
-// your first API endpoint...
-app.get("/api/hello", function(req, res) {
-  res.json({ greeting: "hello API" });
-});
-
-var URLSchema = new mongoose.Schema({
+const URLSchema = new mongoose.Schema({
   original_url: String,
   short_url: String
-}); //
+});
+const URLModel = mongoose.model("URLModel", URLSchema);
 
-var URLModel = mongoose.model("URLModel", URLSchema);
-
-var findOneByURL = (url, done) => {
-  URLModel.findOne({ url: url }, (err, data) => {
-    if (err) return console.log(err);
-    done(data);
-  });
-};
-
-app.post("/api/shorturl/new", function(req, res) {
-  var myURL = new URL(req.body.url);
-  var hostName = myURL.hostname.replace("www.", "");
+app.post("/api/shorturl/new", (req, res) => {
+  const myURL = new URL(req.body.url);
+  const hostName = myURL.hostname.replace("www.", "");
   dns.lookup(hostName, err => {
     if (err) {
       res.json({ error: "invalid URL" });
@@ -71,7 +47,8 @@ app.post("/api/shorturl/new", function(req, res) {
             .exec((err, data) => {
               if (err) return console.error(err);
               var short_url = 1;
-              if (data) short_url = data.short_url++;
+              console.log(data);
+              if (data) short_url = +data.short_url + 1;
               URLModel.create(
                 { original_url: req.body.url, short_url },
                 (err, data) => {
@@ -89,6 +66,16 @@ app.post("/api/shorturl/new", function(req, res) {
   });
 });
 
-app.listen(port, function() {
+app.get("/api/shorturl/:short_url", (req,res)=>{
+  URLModel.findOne({short_url: req.short_url}, (err, data)=>{
+    if(data){
+      
+    }else{
+      res.json({"error":"No short url found for given input"});
+    }
+  });
+});
+
+app.listen(port, () => {
   console.log("Node.js listening ...");
 });
